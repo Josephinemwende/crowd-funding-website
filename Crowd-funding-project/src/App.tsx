cf818b3143160735b37def24469ee9b6a3b75b9a
Josephine Mwende <Josymwesh45@gmail.com>
2024-06-18 21:57:01 +0300

    A react project

---
+++ Crowd-funding-project/src/App.tsx
@@ -0,0 +1,9 @@
+import Navbar from "./navigation";
+const App = () => {
+  return (
+    <>
+      <Navbar />
+    </>
+  );
+};
+export default App;
---
+++ Crowd-funding-project/src/navigation.tsx
@@ -0,0 +1,15 @@
+function Navbar() {
+  return (
+    <>
+      <nav className="navbar">
+        <h1>crowdfund</h1>
+        <img
+          src="/src/Images/icon-hamburger.svg"
+          style={{ height: "20px" }}
+          alt="Menu-icon"
+        ></img>
+      </nav>
+    </>
+  );
+}
+export default Navbar;
